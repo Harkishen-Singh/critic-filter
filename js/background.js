715b0e24bffcfed3e1ee0a272f4eba415cc5785f
muskankhedia <muskan.khedia2000@gmail.com>
2020-10-20 18:20:12 +0530

    setting up travis

--- js/background.js
+++ js/background.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 // initialising the chrome storage
 let initialiseMainMemory = {
   criticlistWords: ['required words']
@@ -10,20 +12,20 @@ chrome.runtime.onInstalled.addListener(setup);
 
 //Declaring options for context-menu
 var menu1 = {
-    id: "addWord",
-    title: "Add this word",
-    contexts: ["selection"]
+  id: 'addWord',
+  title: 'Add this word',
+  contexts: ['selection']
 };
 
 chrome.contextMenus.create(menu1);
 
-chrome.contextMenus.onClicked.addListener( function(clickData,$scope){
-    var text = clickData.selectionText;
-    if (clickData.menuItemId == "addWord" && text) {
-        chrome.storage.local.get("mainMemory", function (details) {
-          console.log("text: ", text);
-          details.mainMemory.criticlistWords.push(text);
-          chrome.storage.local.set({ mainMemory: details.mainMemory });
-        });
-    }
+chrome.contextMenus.onClicked.addListener(function (clickData, $scope) {
+  var text = clickData.selectionText;
+  if (clickData.menuItemId === 'addWord' && text) {
+    chrome.storage.local.get('mainMemory', function (details) {
+      // console.log('text: ', text);
+      details.mainMemory.criticlistWords.push(text);
+      chrome.storage.local.set({ mainMemory: details.mainMemory });
+    });
+  }
 });
