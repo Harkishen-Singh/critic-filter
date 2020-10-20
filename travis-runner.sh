20e524bc8bcf44f83384175de3713f367db6ac7c
Harkishen Singh <harkishensingh@hotmail.com>
2020-10-21 01:23:49 +0530

    Add htmlhint support for travis checks (#5)
    * Add support for htmlhint checks
    
    Signed-off-by: Harkishen-Singh <harkishensingh@hotmail.com>

--- travis-runner.sh
+++ travis-runner.sh
@@ -1,7 +1,14 @@
 echo 'assuming npm is installed along with eslint globally'
+
 echo 'checking lint standards'
 eslint lib/ --color
 echo 'prefect linting'
+
+echo 'checking html lint standards'
+htmlhint lib/ui/**/**/*.html --rules=.htmlhintrc
+htmlhint lib/ui/**/*.html --rules=.htmlhintrc
+echo 'perfect html linting'
+
 echo 'checking code pretty'
 prettier 'lib/scripts/*.js' --check
 echo 'perfect prettifying'
